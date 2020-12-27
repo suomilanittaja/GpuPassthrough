2a4b95386d180610a3fbf5df800f28c4961d510c
Volfase <randomvlog63@gmail.com>
2020-12-27 22:18:25 +0200

    Create polaris-vfio-startup.sh

---
+++ polaris-vfio-startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+# Stop display manager (KDE specific)
+systemctl stop lightdm.service
+
+# Unbind VTconsoles
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+
+# Unbind EFI-Framebuffer
+# echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+sleep 5
+
+# Unload AMD drivers
+modprobe -r amdgpu
+
+# Unbind the GPU from display driver
+virsh nodedev-detach pci_0000_08_00_0
+virsh nodedev-detach pci_0000_08_00_1
+
+# Load VFIO kernel module
+modprobe vfio-pci
