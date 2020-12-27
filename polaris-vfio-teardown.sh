777de50583b7b413446478b225ac32f6a87d34c1
Volfase <randomvlog63@gmail.com>
2020-12-27 22:18:58 +0200

    Create polaris-vfio-teardown.sh

---
+++ polaris-vfio-teardown.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+# Unload VFIO-PCI Kernel Driver
+modprobe -r vfio-pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+
+# Re-Bind GPU to AMD Driver
+virsh nodedev-reattach pci_0000_08_00_1
+virsh nodedev-reattach pci_0000_08_00_0
+
+# Rebind VT consoles
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+
+# Re-Bind EFI-Framebuffer
+# echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+#Load amd driver
+modprobe amdgpu
+
+# Restart Display Manager
+systemctl start lightdm.service
